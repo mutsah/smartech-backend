ddeac44d8bc2e2a7cb688f35660b1eb0241d9c98
Mutsa Taringa <mutsahfx@gmail.com>
2025-06-25 00:48:27 +0200

    Update paymentRoute.js

--- routes/paymentRoute.js
+++ routes/paymentRoute.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const paymentController = require('../controllers/paymentController');
 
-router.post('/create-order', paymentController.createOrder);
-router.post('/capture-order/:orderID', paymentController.captureOrder);
+router.post('/create-order', paymentController.initiatePayment);
+router.post('/payment-result', paymentController.handlePaymentResult);
+// router.post('/capture-order/:orderID', paymentController.captureOrder);
 
 module.exports = router;
